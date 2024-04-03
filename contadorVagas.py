19286b5e0284f0a00f83d103cc523f68d766abf3
RAFAEL KAMINSKI DELLA LIBERA <rafael_libera@estudante.sc.senai.br>
2024-04-02 21:33:40 -0300

    ajustes

--- contadorVagas.py
+++ contadorVagas.py
@@ -17,7 +17,7 @@ video = cv2.VideoCapture('video.mp4')
 while True:
     check,img = video.read()
     imgCinza = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    imgTh = cv2.adaptiveThreshold(imgCinza,255,cv2.ADAPTATIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,25,16)
+    imgTh = cv2.adaptiveThreshold(imgCinza,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,25,16)
     imgBlur = cv2.medianBlur(imgTh,5)
     kernel = np.ones((3,3),np.int8)
     imgDil = cv2.dilate(imgBlur,kernel)
@@ -27,7 +27,7 @@ while True:
 
         recorte = imgDil[y:y+h,x:x+w]
         qtPxBranco = cv2.countNonZero(recorte)
-        cv2.putText(img,str(qtTxBranco),(x,y+h-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        cv2.putText(img,str(qtPxBranco),(x,y+h-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
 
         if qtPxBranco > 3000:
             cv2.rectangle(img, (x,y), (x + w, y + h), (0, 0, 255), 3)
